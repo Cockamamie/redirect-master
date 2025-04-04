7793bcb67041cccea556f9749ac62afc531bcef1
Cockamamie <cock4mamie@gmail.com>
2025-04-05 03:05:41 +0500

    configure rsbuild and add manifest

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,37 @@
-import { defineConfig } from '@rsbuild/core';
+import { defineConfig, rspack } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [
+    pluginReact(),
+  ],
+  source: {
+    entry: {
+      index: './src/index.tsx'
+    },
+  },
+  tools: {
+    htmlPlugin: {
+      title: 'redirect-master',
+    },
+    rspack: {
+      plugins: [
+        new rspack.CopyRspackPlugin({
+          patterns: [
+            'manifest.json',
+            {
+              from: './src/assets',
+              to: 'assets',
+            }
+          ]
+        }),
+      ],
+      module: {
+        rules: []
+      },
+    },
+  },
+  dev: {
+    writeToDisk: true,
+  }
 });
